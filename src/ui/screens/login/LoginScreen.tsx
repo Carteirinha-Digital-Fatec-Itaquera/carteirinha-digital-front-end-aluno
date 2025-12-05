88d558e3f4b35b69bb45141055cfc76c03f7b050
JoaoV-A01 <joaosilva.dev01@gmail.com>
2025-12-05 12:23:21 -0300

    feat: implementar autenticação (incompleto) - Student - 05-12-25

--- src/ui/screens/login/LoginScreen.tsx
+++ src/ui/screens/login/LoginScreen.tsx
@@ -1,21 +1,73 @@
-import { Image, View } from 'react-native';
+import { Image, View, ActivityIndicator, Alert } from 'react-native';
 import { NavigationProps } from '../../../routes';
 import { useNavigation } from '@react-navigation/native';
 import { ButtonComp } from '../../components/button/ButtonComp';
 import { TitleComp } from '../../components/title/TitleComp';
 import { InputComp } from '../../components/input/InputComp';
 import { InputPasswordComp } from '../../components/inputpassword/InputPasswordComp';
-import { useState } from 'react';
+import { useContext, useEffect, useState } from 'react';
 import { backgroundColor } from '../../themes/Color';
 import { TextClickableComp } from '../../components/textclickable/TextClickableComp';
 import { styles } from './style';
 import { SpacerComp } from '../../components/spacer/SpacerComp';
+import AsyncStorage from '@react-async-storage/async-storage';
+import api from '../../../services/api';
+import jwt_decode from 'jwt-decode';
+import { AuthContext } from '../../../contexts/AuthContext';
 
 export default function LoginScreen() {
   const { navigate } = useNavigation<NavigationProps>();
 
   const [email, setEmail] = useState("")
   const [password, setPassword] = useState("")
+  const [loading, setLoading] = useState(false)
+  const { login } = useContext(AuthContext)
+
+  useEffect(() => {
+    const checkLogin = async () => {
+      const token = await AsyncStorage.getItem('@token');
+
+      if (token) {
+        navigate('MainMenu');
+      }
+    };
+    checkLogin();
+  }, []);
+
+  const handleLogin = async () => {
+    
+    if (!email || !password) {
+      Alert.alert('Erro', 'Por favor, preencha todos os campos');
+      return;
+    }
+    setLoading(true);
+
+    try {
+      const response = api.post('/autenticacao/login', {
+        email: email.toLowerCase().trim(),
+        password,
+      });
+      console.log('Resposta do login', response.data);
+
+      if (response.data.status && response.data.content) {
+        const token = response.data.content;
+
+        await login(token);
+
+        navigate('MainMenu')
+      } else {
+        Alert.alert('Erro', response.data.message || 'Credenciais inválidas');
+      }
+    } catch (error) {
+      console.error('Erro no login', error.response?.data || error.message);
+      Alert.alert(
+        'Erro',
+        error.response?.data?.message || 'Falha ao realizar login'
+      )
+    } finally {
+      setLoading(false)
+    }
+  }
 
   return (
     <View style={styles.container}>
