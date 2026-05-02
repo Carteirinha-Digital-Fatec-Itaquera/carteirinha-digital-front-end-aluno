34b184c4502b6623571355c3e5b3875cc8322dac
d-Jhon-b <wwluiza.09@gmail.com>
2026-05-02 01:16:43 -0300

    solve-error_router

--- src/api/config/globalVar.ts
+++ src/api/config/globalVar.ts
@@ -2,5 +2,6 @@ const ADDRESS = "localhost";
 const PORT = "3000";
 
 export const GLOBAL_VAR = {
-  BASE_URL: `http://${ADDRESS}:${PORT}`,
+  // BASE_URL: `http://${ADDRESS}:${PORT}`,
+  BASE_URL: import.meta.env.VITE_API_URL || "http://localhost:3000",
 }
