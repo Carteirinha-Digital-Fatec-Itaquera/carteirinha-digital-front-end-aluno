8812da66879af182bec91e14a3286701a05413ab
d-Jhon-b <wwluiza.09@gmail.com>
2026-05-02 01:12:09 -0300

    att

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-5a5d9309'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.40qehmu1kbg"
+    "revision": "0.15vscsb1f1c"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
