4482dd8ba9b53882029a6c4bf862596962fe10ff
d-Jhon-b <wwluiza.09@gmail.com>
2026-05-04 16:18:18 -0300

    solve3

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-5a5d9309'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.rv3q1efsrp"
+    "revision": "0.h618lfuva7k"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
