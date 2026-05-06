1d1c219fe8a357887e6eed4656fd26a3ec716689
d-Jhon-b <wwluiza.09@gmail.com>
2026-05-06 00:30:09 -0300

    solve1

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-5a5d9309'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.h618lfuva7k"
+    "revision": "0.jb87sdus958"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
