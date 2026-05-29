6d3c4a9921c41ae98db8e77c9546124fb792b7a7
d-Jhon-b <wwluiza.09@gmail.com>
2026-05-28 23:30:12 -0300

    finaltes

--- vite.config.ts
+++ vite.config.ts
@@ -19,19 +19,19 @@ export default defineConfig({
         display: 'standalone', 
         icons: [
           {
-            src: 'pwa-192x192-refatorado.png',
+            src: 'pwa-192x192-end.png',
             sizes: '192x192',
             type: 'image/png',
             purpose: 'any'
           },
           {
-            src: 'pwa-192x192-refatorado.png',
+            src: 'pwa-192x192-end.png',
             sizes: '192x192',
             type: 'image/png',
             purpose: 'maskable'
           },
           {
-            src: 'pwa-512x512-refatorado.png',
+            src: 'pwa-512x512-end.png',
             sizes: '512x512',
             type: 'image/png',
             // purpose: 'any'
